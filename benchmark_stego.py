528479da7b4f98c203f8561d0a70cf9ebafd62a0
Davi Schmitz <davi.schmitz@sap.com>
2024-10-06 00:47:46 -0300

    Benchmark different embedding scales

--- benchmark_stego.py
+++ benchmark_stego.py
@@ -1,36 +1,38 @@
 import subprocess
 
-# Define the paths to the scripts
 steganography_script = "steganography_higher_band.py"
 metrics_script = "metrics.py"
 
-# Define the wavelet types to test
 wavelet_types = ["haar", "db1", "db8", "db16", "sym2", "sym8", "sym16"]
 
 cover_image_path = "lena_std.tif"
 hidden_image_path = "qrcode_compact_512x512.tif"
 stego_image_path = "stego.tif"
 
-# Define the embedding scale
-embed_scale = 0.02  # You can adjust this value as needed
+embedding_scales = [0.02, 0.05, 0.1, 0.2]
 
 for wavelet_type in wavelet_types:
-    print(f"Running steganography script with wavelet type: {wavelet_type}...")
-    steganography_process = subprocess.run(
-        [
-            "python3",
-            steganography_script,
-            "--cover_image",
-            cover_image_path,
-            "--hidden_image",
-            hidden_image_path,
-            "--stego_image",
-            stego_image_path,
-            "--wavelet_type",
-            wavelet_type,
-            "--embed_scale",
-            str(embed_scale),
-        ],
-        check=True,
-    )
-    print(f"Steganography script completed for wavelet type: {wavelet_type}.")
+    for embedding_scale in embedding_scales:
+        print(
+            f"Running steganography script with wavelet type: {wavelet_type} and embedding scale: {embedding_scale}"
+        )
+        steganography_process = subprocess.run(
+            [
+                "python3",
+                steganography_script,
+                "--cover_image",
+                cover_image_path,
+                "--hidden_image",
+                hidden_image_path,
+                "--stego_image",
+                stego_image_path,
+                "--wavelet_type",
+                wavelet_type,
+                "--embed_scale",
+                str(embedding_scale),
+            ],
+            check=True,
+        )
+        print(
+            f"Steganography script completed for wavelet type: {wavelet_type} and embedding scale: {embedding_scale}"
+        )
