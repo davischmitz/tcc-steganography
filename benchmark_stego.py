22a427ce4f76f13b193c8ea58f43997c02c65f48
Davi Schmitz <davi.schmitz@sap.com>
2024-11-17 16:42:40 -0300

    Update benchmark script

--- benchmark_stego.py
+++ benchmark_stego.py
@@ -1,17 +1,19 @@
 import subprocess
 
-high_frequency_stego_script = "steganography_higher_band.py"
 low_frequency_stego_script = "steganography_lower_band.py"
+high_frequency_stego_script = "steganography_higher_band.py"
 multilevel_stego_script = "steganography_higher_band_multilevel_v2.py"
-metrics_script = "metrics.py"
 
 wavelet_types = ["haar", "db1", "db8", "db16", "sym2", "sym8", "sym16"]
 
-cover_image_path = "lena_std.tif"
-hidden_image_path = "qrcode_compact_512x512.tif"
+cover_image_path = "peppers_color.tif"
+hidden_image_path = "qrcode_512x512.tif"
 stego_image_path = "stego.tif"
 
 embedding_scales = [0.02, 0.05, 0.1, 0.2]
+
+multilevel_dwt_embed_level = 2
+multilevel_dwt_extract_level = 2
 
 for wavelet_type in wavelet_types:
     for embedding_scale in embedding_scales:
@@ -61,6 +63,9 @@ for wavelet_type in wavelet_types:
         print(
             f" High frequency steganography script completed for wavelet type: {wavelet_type} and embedding scale: {embedding_scale}"
         )
+        print(
+            f"Running multilevel steganography script with wavelet type: {wavelet_type} and embedding scale: {embedding_scale}"
+        )
         steganography_process = subprocess.run(
             [
                 "python3",
@@ -75,6 +80,13 @@ for wavelet_type in wavelet_types:
                 wavelet_type,
                 "--embed_scale",
                 str(embedding_scale),
+                "--embed_dwt_level",
+                str(multilevel_dwt_embed_level),
+                "--extract_dwt_level",
+                str(multilevel_dwt_extract_level),
             ],
             check=True,
+        )
+        print(
+            f"Multilevel steganography script completed for wavelet type: {wavelet_type} and embedding scale: {embedding_scale}"
         )
